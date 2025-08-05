3eb7d8482ffb63b20e96644f1b8f05c6a10edac8
Ranjith <ejm1cob@bosch.com>
2025-08-05 16:24:49 +0530

    changes

--- stats.py
+++ stats.py
@@ -2,7 +2,7 @@
 def calculateStats(numbers):
   # implement the computation of statistics here and return the results
   stats['avg'] = sum(numbers) / len(numbers)
-  stats['min'] = min(numbers) 
-  stats['max'] = max(numbers) 
+  stats['max'] = max(numbers)
+  stats['min'] = min(numbers)  
   return stats
   return None
