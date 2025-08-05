419bb195b8092620100dde7a03c049a1d974e2e0
Ranjith <ejm1cob@bosch.com>
2025-08-05 16:49:37 +0530

    change2

--- stats.test.py
+++ stats.test.py
@@ -17,7 +17,7 @@ class StatsTest(unittest.TestCase):
     self.assertTrue(math,isnan(computedstats['min']))
 
   def test_avg_ignore_nan_in_input(self):
-  computedStats = statisctics.CaculateStats([1.5,float('nan'),3.2,4.5])
+  computedStats = statisctics.CalculateStats([1.5,float('nan'),3.2,4.5])
   epsilon = 0.001
     self.assertAlmostEqual(computedStats["avg"], 3.067, delta=epsilon)
     self.assertAlmostEqual(computedStats["max"], 4.5, delta=epsilon)
