129e16855131e0c2423b7d873a62eb0ffae14660
Ranjith <ejm1cob@bosch.com>
2025-08-12 10:18:27 +0530

    error_correction5

--- stats.test.py
+++ stats.test.py
@@ -13,12 +13,12 @@ class StatsTest(unittest.TestCase):
 
   def test_avg_is_nan_for_empty_input(self):
     computedStats = statistics.calculateStats([])
-    self.assertTrue(math,isnan(computedstats['avg']))
-    self.assertTrue(math,isnan(computedstats['max']))
-    self.assertTrue(math,isnan(computedstats['min']))
+    self.assertTrue(math.isnan(computedstats['avg']))
+    self.assertTrue(math.isnan(computedstats['max']))
+    self.assertTrue(math.isnan(computedstats['min']))
 
   def test_avg_ignore_nan_in_input(self):
-    computedStats = statistics.calculateStats([1.5,float('nan'),3.2,4.5])
+    computedStats = statistics.calculateStats([1.5, float('nan'), 3.2, 4.5])
     epsilon = 0.001
     self.assertAlmostEqual(computedStats["avg"], 3.067, delta=epsilon)
     self.assertAlmostEqual(computedStats["max"], 4.5, delta=epsilon)
@@ -26,15 +26,15 @@ class StatsTest(unittest.TestCase):
 
   def test_avg_nan_for_all_nan_input(self):
     computedStats = statistics.calculateStats([float('nan'),float('nan')])
-    self.assertTrue(math,isnan(computedstats['avg']))
-    self.assertTrue(math,isnan(computedstats['max']))
-    self.assertTrue(math,isnan(computedstats['min']))
+    self.assertTrue(math.isnan(computedstats['avg']))
+    self.assertTrue(math.isnan(computedstats['max']))
+    self.assertTrue(math.isnan(computedstats['min']))
 
   def test_avgw_nan_for_absurd_input(self):
-    computedStats = statistics.calculateStats([1e10,-1,9.2,4.5,1e5])
-    self.assertTrue(math,isnan(computedstats['avg']))
-    self.assertTrue(math,isnan(computedstats['max']))
-    self.assertTrue(math,isnan(computedstats['min']))
+    computedStats = statistics.calculateStats([1e10, -1, 9.2, 4.5, 1e5])
+    self.assertTrue(math.isnan(computedstats['avg']))
+    self.assertTrue(math.isnan(computedstats['max']))
+    self.assertTrue(math.isnan(computedstats['min']))
 
 
 
