72970f9288ed42a7238a72b7f03e497ee51e2c13
Ranjith <ejm1cob@bosch.com>
2025-08-12 09:59:35 +0530

    error correction_3

--- stats.py
+++ stats.py
@@ -6,7 +6,7 @@ def calculateStats(numbers):
   stats ={}
   if numbers:
    numbers_without_nan =[num for num in numbers if not math.isnan(num)]
-  if numbers_without_nan and is_within_range(numbers_without-nan):
+  if numbers_without_nan and is_within_range(numbers_without_nan):
    stats['avg'] = sum(numbers_without_nan)/len(numbers_without_nan)
    stats['max'] = max(numbers_without_nan)
    stats['min'] = min(numbers_without_nan) 
