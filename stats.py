402e7b03a0a2752468c94c9ace638c1bbd693899
Ranjith <ejm1cob@bosch.com>
2025-08-12 10:29:17 +0530

    ERROR_CHANGES_7

--- stats.py
+++ stats.py
@@ -3,9 +3,8 @@ def is_within_range(numbers):
    return all(num > 0 and num < 200 for num in numbers)
 def calculateStats(numbers):
   # implement the computation of statistics here and return the results
-  stats ={}
-  if numbers:
-   numbers_without_nan =[num for num in numbers if not math.isnan(num)]
+  stats = {}
+  numbers_without_nan =[num for num in numbers if not math.isnan(num)]
   if numbers_without_nan and is_within_range(numbers_without_nan):
    stats['avg'] = sum(numbers_without_nan)/len(numbers_without_nan)
    stats['max'] = max(numbers_without_nan)
